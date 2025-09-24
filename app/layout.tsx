fce2a8585f4e27e6c4ae84a7607c5c8c11e4fe72
Fedor Kobyakov <fedor.kobyakov@onlyoffice.com>
2025-09-24 14:14:56 +0300

    fix double scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Habbits tracker",
@@ -25,11 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > */}
       <body
-        className=""
+        style={{ height: "initial" }}
       >
         <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID || ""}>
           {children}
